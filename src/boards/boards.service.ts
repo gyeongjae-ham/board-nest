8544dbee30e2ab47cb702652dd7ce8aebf2e892a
hiyee-gj <gyeongjae.h.dev@gmail.com>
2021-12-22 11:46:31 +0900

    Refactor repository pattern.

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -16,17 +16,8 @@ export class BoardsService {
   //   return this.boards;
   // }
 
-  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
-    const { title, description } = createBoardDto;
-
-    const board = this.boardRepository.create({
-      title,
-      description,
-      status: BoardStatus.PUBLIC,
-    });
-
-    await this.boardRepository.save(board);
-    return board;
+  createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+    return this.boardRepository.createBoard(createBoardDto);
   }
 
   // createBoard(createBoardDto: CreateBoardDto) {
