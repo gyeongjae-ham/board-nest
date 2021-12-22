11a086902ef96f37c5c775af7e584a2521e89afd
hiyee-gj <gyeongjae.h.dev@gmail.com>
2021-12-22 18:07:00 +0900

    Refactor env settings.

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+
+const dbConfig = config.get('db');
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'mslfl4861!!',
-  database: 'boardproject',
+  type: dbConfig.type,
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
+  port: process.env.RDS_PORT || dbConfig.port,
+  username: process.env.RDS_USERNMAE || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.RDS_DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: dbConfig.synchronize,
 };
